524683dcffd75a2dcba902ce0295333db9552a85
David Goodlad <david@goodlad.ca>
2010-05-05 12:46:09 +1000

    Add minimal docs, remove silly copy-paste

--- lib/dollhouse/remote_server.rb
+++ lib/dollhouse/remote_server.rb
@@ -7,15 +7,22 @@ module Dollhouse
     include Dollhouse::Tasks::Babushka
 
     attr_reader :ssh
+
+    # Connect to a remote server, and execute the given block within the
+    # context of that server.
+    # If you don't supply a password, pubkey authentication should take over.
+    def self.connect(host, user, password = nil, &block)
+      puts "Connecting to #{host} as #{user}..."
+      Net::SSH.start(host, user, :password => password) do |ssh|
+        new(ssh).instance_eval(&block)
+      end
+    end
     
     def initialize(ssh)
       @ssh = ssh
     end
     
-    def log(message)
-      puts message
-    end
-
+    # Write to a remote file at _path_.
     def write_file(path)
       Tempfile.open(File.basename(path)) do |f|
         yield f
@@ -49,60 +56,5 @@ module Dollhouse
     def get_environment(var)
       @ssh.exec!("echo $#{var}").strip
     end
-    
-    def self.connect(host, user, password, &block)
-      puts "Connecting to #{host} as #{user}..."
-      Net::SSH.start(host, user, :password => password) do |ssh|
-        server = new(ssh)
-        
-        server.instance_eval(&block)
-      end
-    end
-    
-    def self.set_root_password(host, username, password)
-      connect(host, username, password) do
-        log.info "set root password" do
-          ssh.exec! "sudo passwd root" do |ch, stream, data|
-            if data =~ /password for #{username}\:/ || data =~ /(Enter|Retype) new UNIX password\:/
-              ch.send_data("#{password}\n")
-            elsif data =~ /password updated successfully/
-              log.info :good, "  password changed."
-            else
-              unless data =~ /\A\s*\Z/
-                log.error data
-                exit 1
-              end
-            end
-          end
-        end
-      end
-    end
-    
-    def install_runtime_environment(mirror)
-      log.info "install runtime environment" do
-        log.info "setup sources.list for #{mirror}"
-        upload_template 'templates/sources.list.erb', '/etc/apt/sources.list', :mirror => mirror
-
-        log.info "apt-get update" do
-          # This is running locally so can't alter the environment
-          # ENV['DEBIAN_FRONTEND'] = 'noninteractive'
-          exec_command 'apt-get update -y', true
-        end
-
-        log.info "apt-get upgrade" do
-          # This is running locally so can't alter the environment
-          #ENV['DEBIAN_FRONTEND'] = 'noninteractive'
-          exec_command 'apt-get upgrade -y', true
-        end
-
-        install_ree_from_package
-        # install_rvm
-        # install_ree_from_rvm
-
-        log.info "install other packages" do
-          exec_command 'apt-get install -y pcregrep pwgen', true
-        end
-      end
-    end  
-  end
+  end    
 end
