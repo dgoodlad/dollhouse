7bbc20968c41df231bafaa3ee2965f73b54c7107
David Goodlad <david@goodlad.ca>
2010-05-11 16:06:30 +1000

    Handle password prompts

--- lib/dollhouse/remote_server.rb
+++ lib/dollhouse/remote_server.rb
@@ -4,6 +4,8 @@ require 'tempfile'
 
 module Dollhouse
   class RemoteServer
+    include Net::SSH::Prompt
+
     include Dollhouse::Tasks::Babushka
     include Dollhouse::Tasks::Bootstrap
 
@@ -12,17 +14,17 @@ module Dollhouse
     # Connect to a remote server, and execute the given block within the
     # context of that server.
     # If you don't supply a password, pubkey authentication should take over.
-    def self.connect(host, user, password = nil, &block)
+    def self.connect(host, user, options = {}, &block)
       puts "Connecting to #{host} as #{user}..."
-      Net::SSH.start(host, user, :password => password) do |ssh|
+      Net::SSH.start(host, user, options) do |ssh|
         new(ssh).instance_eval(&block)
       end
     end
-    
+
     def initialize(ssh)
       @ssh = ssh
     end
-    
+
     # Write to a remote file at _path_.
     def write_file(path)
       Tempfile.open(File.basename(path)) do |f|
@@ -31,29 +33,33 @@ module Dollhouse
         @ssh.sftp.upload!(f.path, path)
       end
     end
-    
+
     def exec(command)
       output = ""
-      
+
       @ssh.exec!("(#{command}) && echo SUCCESS || echo FAILURE $?") do |ch, stream, data|
-        if stream == :stderr
-          puts "ERR: #{data}"
-        else # stdout
-          output << data
+        case stream
+        when :stderr
+          print data
+        when :stdout
+          if data =~ /[Pp]assword.+:/
+            ch.send_data("#{prompt(" ", false)}\n")
+          else
+            output << data
+            print data
+          end
         end
       end
-      
+
       if output =~ /\A(.*)(SUCCESS|FAILURE)( \d+)?\n\Z/m
         result = [$2 == 'SUCCESS', $1, $3.to_i]
       else
         raise "weird #{output.inspect}"
       end
 
-      puts output
-      
       block_given? ? yield(result) : result
     end
-    
+
     def get_environment(var)
       @ssh.exec!("echo $#{var}").strip
     end
@@ -61,5 +67,5 @@ module Dollhouse
     def connected_as_root?
       @ssh.exec!("id") =~ /^uid=0\(root\)/
     end
-  end    
+  end
 end
